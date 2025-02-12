222f0be45c73e95b686469ce405c392b7fef0cc0
syedmehedi34 <syedmeehdi34@gmail.com>
2025-02-13 01:41:22 +0600

    Recent Blog Posts

--- src/components/RecentBlogs.jsx
+++ src/components/RecentBlogs.jsx
@@ -41,10 +41,10 @@ const RecentBlogs = () => {
   return (
     <section className="py-16 w-11/12 mx-auto">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-gray-900 mb-4 text-center">
+        <h2 className="text-3xl font-bold text-gray-900 mb-4 text-center dark:text-gray-50">
           Recent Blog Posts
         </h2>
-        <p className="text-xl font-[500] text-gray-900 mb-12 text-center">
+        <p className="text-xl font-[500] text-gray-900 mb-12 text-center dark:text-gray-50">
           Fresh Insights, Timeless Inspiration –{" "}
           <span className="text-red-600">Dive into Our Recent Blogs!</span>
         </p>
@@ -58,9 +58,10 @@ const RecentBlogs = () => {
                 <img
                   src={blog.image}
                   alt={blog.title}
-                  className="w-full h-48 object-cover"
+                  className="w-full h-48 object-cover dark:bg-gray-800"
                 />
-                <button
+                {/* // todo : implement later */}
+                {/* <button
                   className="absolute top-4 right-4 p-2 bg-white rounded-full shadow-md hover:bg-gray-100 transition-colors"
                   title="Add to Wishlist"
                 >
@@ -78,13 +79,17 @@ const RecentBlogs = () => {
                       d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                     />
                   </svg>
-                </button>
+                </button> */}
               </div>
-              <div className="p-6 flex flex-col h-[300px]">
-                <h3 className="text-xl font-semibold text-gray-900 mb-2">
-                  <p className="hover:text-blue-600">{blog.title}</p>
+              <div className="p-6 flex flex-col h-[300px] dark:bg-gray-800">
+                <h3 className="text-xl font-semibold text-gray-900 mb-2 dark:text-gray-50">
+                  <p className="hover:text-blue-600 dark:text-gray-50">
+                    {blog.title}
+                  </p>
                 </h3>
-                <p className="text-gray-600 mb-4">{blog.short_description}</p>
+                <p className="text-gray-600 mb-4 dark:text-gray-50">
+                  {blog.short_description}
+                </p>
 
                 <div className="flex-1 flex-grow">
                   <div className="flex items-center mb-4">
@@ -94,13 +99,13 @@ const RecentBlogs = () => {
                         "https://img.icons8.com/office/40/person-male-skin-type-4.png"
                       }
                       alt={blog.authorName}
-                      className="w-10 h-10 rounded-full mr-3 border border-gray-300"
+                      className="w-10 h-10 object-cover rounded-full mr-3 border border-gray-300 "
                     />
                     <div>
-                      <p className="text-sm font-medium text-gray-900">
+                      <p className="text-sm font-medium text-gray-900 dark:text-gray-50">
                         {blog.authorName}
                       </p>
-                      <p className="text-sm text-gray-500">
+                      <p className="text-[12px] text-gray-500 dark:text-gray-400">
                         {getTimeAgo(blog.submissionTime)}
                       </p>
                     </div>
